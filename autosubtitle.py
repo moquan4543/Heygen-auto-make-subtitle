fda4f791b42bd01f762c1ad79e5f8e96038aa1f0
moquan4543 <117621269+moquan4543@users.noreply.github.com>
2023-05-24 13:11:08 +0800

    Update autosubtitle.py

--- autosubtitle.py
+++ autosubtitle.py
@@ -1,12 +1,3 @@
-'''
-流程:
-1.剪映輸出字幕選txt檔
-2.可以利用馴碼快手轉成繁體中文(option)
-3.手動根據老師講的內容分好哪幾句話是同一頁PPT中講的，不同頁的內容用連續10至50個字母'a'來分隔
-4.將改好的txt檔讀入這隻程式裡(在main中指定好路徑)
-5.手動將進度條拉至第一張投影片，注意需要有add speech字樣
-6.執行程式
-'''
 import pyautogui as pg
 import time
 import os.path
